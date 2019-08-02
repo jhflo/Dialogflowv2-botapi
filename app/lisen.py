8cd1fd303e442d47ddc74e7b678e1c5540cb1063
jhflo <jorgeflorez28@hotmail.com>
2019-08-02 16:31:06 -0500

    Add files via upload

---
+++ app/lisen.py
@@ -0,0 +1,32 @@
+
+import json
+from flask import Flask
+from flask import request
+from flask import jsonify
+from .nplv2 import nlpprocess
+
+app = Flask(__name__)
+#app.run(host='0.0.0.0')
+
+#Ruta de flask que escucha el Webhook de Cisco Spark creado para el bot
+@app.route('/dialogflow', methods=['POST'])
+def index():
+    """
+    Recoleccion de la informacion del Webhook
+
+    """
+    webhook = request.data
+    webhook = json.loads(webhook)
+    datos = nlpprocess(webhook)
+    return jsonify(datos)
+
+
+
+
+
+
+####Variables fijas AbaCoXBot#####
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ app/nplv2.py
@@ -0,0 +1,31 @@
+
+class nlpprocess:
+
+    def __init__(self, data):
+        self.data = data
+
+    def displaydata(self):
+        print(self.data)
+
+    def processdata(self):
+        import dialogflow_v2 as dialogflow
+        from google.oauth2 import service_account
+        from protobuf_to_dict import protobuf_to_dict
+        credentials = service_account.Credentials.from_service_account_file('/app/credentials/{}.json'.
+                                                                            format(self.data["project_id"]))
+        session_client = dialogflow.SessionsClient(credentials=credentials)
+        session = session_client.session_path(self.data["project_id"], self.data["session_id"])
+        text_input = dialogflow.types.TextInput(text=self.data["text"], language_code=self.data["lan"])
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(session=session, query_input=query_input)
+        action = response.query_result.action
+        text_resp = response.query_result.fulfillment_text
+        parameters = protobuf_to_dict(response.query_result.parameters)
+        return {"action":action, "response":text_resp, "parameters":parameters}
+
+
+datos = nlpprocess({"project_id":1, "session_id":2})
+datos.displaydata()
+
+
+
